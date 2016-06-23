0477f82c0f7c6ba1b0a0bb80888d0d7030dfe917
Jukka Puranen <jukka.puranen@gmail.com>
2016-06-23 16:20:25 +0300

    working on signup form

--- web/elm/Pages/SignUpForm/View.elm
+++ web/elm/Pages/SignUpForm/View.elm
@@ -2,15 +2,20 @@ module Pages.SignUpForm.View exposing (view)
 import Html exposing (..)
 import Html.Attributes exposing (..)
 import Pages.LayoutTemplates.Master as Master
-import App.Common exposing (Msg(..), Page(..))
 import App.Update exposing (Model)
+import App.Common exposing (Msg(..))
 
-view : Model ->  Html App.Common.Msg
 
-view model =
-    Master.view [] [signUpForm] []
+view : Model -> Html Msg
 
-signUpForm : Html a 
+view model  =
+    Master.view [] [ signUpForm model ] []
 
-signUpForm = 
-    div [] [text "signup form"]
+signUpForm : Model -> Html Msg 
+
+signUpForm model = 
+    div [ class "input-field" ] 
+        [
+            input [ id "email", type' "email", class "validate" ] [],
+            label [ for "email"] [ text "Email" ]
+        ]
