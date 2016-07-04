30b27d460c18f8b816ec9b29a8d4bccc9d3875fe
Jukka Puranen <jukka.puranen@gmail.com>
2016-07-04 15:49:40 +0300

    implemented auth plug

--- web/controllers/user_controller.ex
+++ web/controllers/user_controller.ex
@@ -2,9 +2,23 @@ defmodule PhoenixAuthKata.UserController do
     use PhoenixAuthKata.Web, :controller
 
     def show(conn, %{"id" => id}) do
-        user = Repo.get(PhoenixAuthKata.User, id)
-        json(conn, user)
+        case authenticate(conn) do
+            %Plug.Conn{halted: true} = conn ->
+                conn
+            conn ->
+                user = Repo.get(PhoenixAuthKata.User, id)
+                json(conn, user)
     end
 
+    defp authenticate(conn) do
+        if conn.assigns.current_user do
+            conn
+        else
+            conn 
+            |> put_status(401)
+            |> json %{ok: false}
+            |> halt()
+        end
+    end
 
 end
